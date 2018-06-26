68f381971bde3aa83eb2262148d399e916387e29
Jon Schulman <schulman99@gmail.com>
2018-06-26 02:01:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(call)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |e| x.length>4 }
 end
 
 def find_the_cheese# code an argument here
